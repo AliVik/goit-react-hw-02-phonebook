665ac5eef09c72a39a40de1b5a9b59881817e723
AliVik <alvikul1994@gmail.com>
2022-01-11 16:28:57 +0200

    добавляет папки и файлы компонентов формы и списка

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
-function App() {}
+import React, { Component } from "react";
+
+
+class App extends Component {
+    state = {
+        contacts: [],
+        name: ''
+    }
+
+    render() { }
+}
 
 export default App;
