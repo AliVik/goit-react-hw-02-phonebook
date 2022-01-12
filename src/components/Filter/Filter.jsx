8e4f099add28d3df0505bacae65a67e21ac6aa7c
AliVik <alvikul1994@gmail.com>
2022-01-12 22:30:41 +0200

    добавляет компонент фильтра

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,20 @@
+import { Component } from "react";
+
+function Filter ({value,onChange}){
+   
+        return <label>
+                    Find contacts by name
+                    <input
+                        type="text"
+                        name="filter"
+                        value={value}
+                        onChange={onChange}
+                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                        required
+                    />
+            </label>
+    
+}
+
+export default Filter;
