5435dd8563b618746efbc075283f57ae85f46673
AliVik <alvikul1994@gmail.com>
2022-01-12 22:32:10 +0200

    создаёт набросок функционала фильтра

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import Form from "./components/Form";
 import ContactList from "./components/ContactList";
+import Filter from "./components/Filter";
 
 
 class App extends Component {
     state = {
-        contacts: [],
+        contacts: [  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
         filter: '',
 
     }
@@ -13,26 +17,27 @@ class App extends Component {
 
 
     handleFormDatas = (data) => {
-        console.log(data)
-
+    
         this.setState(prevState => {
-            // console.log(prevState)
-            // console.log(this.state.name)
-            // return {
-            //     name: [...prevState.name, data.inputName],
-            //     contacts: [...prevState.contacts, data.inputNumber]
-            // }
-
             return {
                 contacts: [...prevState.contacts, data]
             }
         })
     }
 
+    handleFilterDatas = (evt) => {
+        this.setState({ filter: evt.currentTarget.value })
+        this.state.contacts.filter(contact => { 
+            console.log(Array.from(contact.name)===evt.currentTarget.value);
+            // return contact.name.splice('') === evt.currentTarget.value;
+        })
+    }
+
     render() {
         return <>
             <Form onSubmit={this.handleFormDatas} />
-            <ContactList contacts={this.state.contacts} />
+            <Filter value={this.state.filter} onChange={this.handleFilterDatas} />
+            <ContactList contacts={this.state.contacts}/>
         </>
     }
 }
