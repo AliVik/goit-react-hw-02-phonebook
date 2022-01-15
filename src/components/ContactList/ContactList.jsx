a522b31d2dcd37158adbb1cb5e5d0c92d759b26f
AliVik <alvikul1994@gmail.com>
2022-01-15 19:04:31 +0200

    добавляет кнопку удаления контакта

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,16 @@
 
 
-export default function ContactList({ contacts }) {
-
+export default function ContactList({ contacts,onDeleteClick }) {
+    
+  
     return <>
         <ul>
             {contacts.map(contact => {
-                return <li key={contact.id}>{contact.name}: {contact.number}</li>
+                return <li key={contact.id}>
+                    <span>{contact.name}: {contact.number}</span>
+                    <button type="button" onClick={()=>onDeleteClick(contact.id)}>Delete</button>
+                    </li>
+                
             })}
         </ul>
     </>
